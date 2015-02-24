01cbb4bf05a210bec4816fd3739bf7ecb216bf98
LucKy232 <ciobanu.lucian.gabriel@gmail.com>
2015-02-24 17:53:41 +0100

    Script

---
+++ Assets/Scripts/Station.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Station : MonoBehaviour {
+
+	List<TradeResource> resList = new List<TradeResource>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		TradeResource rawiron = new TradeResource ("Raw Iron",120,0.80f,1.20f);
+		resList.Add (rawiron);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/TradeResource.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TradeResource
+{
+	string name;
+	int amount;				//in tonnes (metric ton)
+	float sellCost;
+	float buyCost;
+
+	// Use this for initialization
+	public TradeResource () 
+	{
+		name = null;
+		amount = 0;
+		sellCost = 0.00f;
+		buyCost = 0.00f;
+	}
+
+	public TradeResource (string resName, int available, float buy, float sell) 
+	{
+		name = resName;
+		amount = available;
+		buyCost = buy;
+		sellCost = sell;
+	}
+
+	public void SetSellCost(float x)
+	{
+		sellCost = x;
+	}
+
+	public void ChangeSellCost(float x)
+	{
+		sellCost += x;
+	}
+
+	public void SetBuyCost(float x)
+	{
+		buyCost = x;
+	}
+	
+	public void ChangeBuyCost(float x)
+	{
+		buyCost += x;
+	}
+
+	//a ship buys from the station
+	public float Buy(int x)		//input: amount; output: money
+	{
+		float money;
+
+		if (x < amount) {
+			amount -= x;
+			money = buyCost * x;
+		} else {
+			amount = 0;
+			money = buyCost * amount;
+		}
+
+		return money;
+	}
+
+	//a ship sells to the station
+	public void Sell(int x)
+	{
+		if(x > 0)
+			amount += x;
+	}
+
+	public string GetName()
+	{
+		return name;
+	}
+}
